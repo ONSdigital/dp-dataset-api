baada41b48ff691fbd41f805091d05a0708519fd
Carl Hembrough <carl@bitwaves.co.uk>
2017-09-11 12:12:01 +0100

    Remove authentication from get dimensions endpoint. It was added by mistake.

--- api/api.go
+++ api/api.go
@@ -46,7 +46,7 @@ func CreateDatasetAPI(secretKey string, router *mux.Router, dataStore store.Data
 	api.router.HandleFunc("/instances/{id}/inserted_observations/{inserted_observations}", api.privateAuth.Check(instance.UpdateObservations)).Methods("PUT")
 
 	dimension := dimension.Store{api.dataStore.Backend}
-	api.router.HandleFunc("/instances/{id}/dimensions", api.privateAuth.Check(dimension.GetNodes)).Methods("GET")
+	api.router.HandleFunc("/instances/{id}/dimensions", dimension.GetNodes).Methods("GET")
 	api.router.HandleFunc("/instances/{id}/dimensions/{dimension}/options", dimension.GetUnique).Methods("GET")
 	api.router.HandleFunc("/instances/{id}/dimensions/{dimension}/options/{value}", api.privateAuth.Check(dimension.Add)).Methods("PUT")
 	api.router.HandleFunc("/instances/{id}/dimensions/{dimension}/options/{value}/node_id/{node_id}", api.privateAuth.Check(dimension.AddNodeID)).Methods("PUT")
