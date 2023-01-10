3b9644296a835b2a5ed27671713ba84bb6edbf14
Valentina <vmilitaru28@gmail.com>
2023-01-10 16:20:00 +0000

    formats mongo/dataset_store.go file

--- mongo/dataset_store.go
+++ mongo/dataset_store.go
@@ -287,31 +287,31 @@ func (m *Mongo) UpdateDataset(ctx context.Context, id string, dataset *models.Da
 }
 
 // UpdateDatasetV2 it is used by the v2 PutDataset endpoint and updates the complete existing dataset document
-func (m *Mongo) UpdateDatasetV2(ctx context.Context,currentDataset *models.DatasetUpdate, updatedDataset *models.Dataset, eTagSelector string) (newETag string,err error) {
+func (m *Mongo) UpdateDatasetV2(ctx context.Context, currentDataset *models.DatasetUpdate, updatedDataset *models.Dataset, eTagSelector string) (newETag string, err error) {
 	// generate a new unique ETag for the dataset
 	newETag, err = newETagForDatasetUpdate(currentDataset, updatedDataset)
 	if err != nil {
 		log.Error(ctx, "error generating a new unique ETag for dataset", err)
 		return "", err
 	}
-	
+
 	updatedDataset.LastUpdated = time.Now()
 
 	sel := datasetSelector(currentDataset.Next.ID, bsonprim.Timestamp{}, eTagSelector)
 
 	update := bson.M{
 		"$set": bson.M{
-			"next":  updatedDataset,
-			"e_tag": newETag,
+			"next":         updatedDataset,
+			"e_tag":        newETag,
 			"last_updated": time.Now(),
 		},
 	}
 
 	if _, err = m.Connection.Collection(m.ActualCollectionName(config.DatasetsCollection)).Must().Update(ctx, sel, update); err != nil {
 		if errors.Is(err, mongodriver.ErrNoDocumentFound) {
-			return "",errs.ErrDatasetNotFound
+			return "", errs.ErrDatasetNotFound
 		}
-		return "",err
+		return "", err
 	}
 
 	return newETag, nil
