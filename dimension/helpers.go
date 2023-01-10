843f6b8c5e55979958a9efb459fb746eebebf73e
Valentina <vmilitaru28@gmail.com>
2023-01-10 17:03:31 +0000

    reverts the changes made to the handleDimensionErr func - not in scope of the current 1351 ticket

--- dimension/helpers.go
+++ dimension/helpers.go
@@ -2,6 +2,7 @@ package dimension
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"io"
 	"io/ioutil"
@@ -10,7 +11,14 @@ import (
 
 	errs "github.com/ONSdigital/dp-dataset-api/apierrors"
 	"github.com/ONSdigital/dp-dataset-api/models"
+	dprequest "github.com/ONSdigital/dp-net/v2/request"
+	"github.com/ONSdigital/log.go/v2/log"
 	"github.com/pkg/errors"
+)
+
+const (
+	reqUser   = "req_user"
+	reqCaller = "req_caller"
 )
 
 // Regex const for patch paths
@@ -87,6 +95,47 @@ func unmarshalDimensionCache(reader io.Reader) (*models.CachedDimensionOption, e
 	}
 
 	return &option, nil
+}
+
+func handleDimensionErr(ctx context.Context, w http.ResponseWriter, err error, data log.Data) {
+	if data == nil {
+		data = log.Data{}
+	}
+
+	var status int
+
+	// Switch by error type
+	switch err.(type) {
+	case errs.ErrInvalidPatch:
+		status = http.StatusBadRequest
+	default:
+		// Switch by error message
+		switch {
+		case errs.NotFoundMap[err]:
+			status = http.StatusNotFound
+		case errs.BadRequestMap[err]:
+			status = http.StatusBadRequest
+		case errs.ConflictRequestMap[err]:
+			status = http.StatusConflict
+		default:
+			status = http.StatusInternalServerError
+			err = errors.WithMessage(err, "internal error")
+		}
+	}
+
+	data["response_status"] = status
+	logError(ctx, err, data)
+	http.Error(w, err.Error(), status)
+}
+
+func logError(ctx context.Context, err error, data log.Data) {
+	if user := dprequest.User(ctx); user != "" {
+		data[reqUser] = user
+	}
+	if caller := dprequest.Caller(ctx); caller != "" {
+		data[reqCaller] = caller
+	}
+	log.Error(ctx, "unsuccessful request", err, data)
 }
 
 func setJSONPatchContentType(w http.ResponseWriter) {
--- dimension/helpers_test.go
+++ dimension/helpers_test.go
@@ -1,10 +1,14 @@
 package dimension
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
 
 	errs "github.com/ONSdigital/dp-dataset-api/apierrors"
+	"github.com/ONSdigital/log.go/v2/log"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -38,5 +42,48 @@ func TestUnmarshalDimensionCache(t *testing.T) {
 			So(err, ShouldResemble, errs.ErrMissingParameters)
 			So(option, ShouldBeNil)
 		})
+	})
+}
+
+type contextKey string
+
+const requestID = contextKey("request_id")
+
+func TestHandleDimensionErr(t *testing.T) {
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, requestID, "123789")
+
+	t.Parallel()
+	Convey("Correctly handle dimension not found", t, func() {
+		w := httptest.NewRecorder()
+		dimensionError := errs.ErrDimensionNotFound
+		logData := log.Data{"test": "not found"}
+
+		handleDimensionErr(ctx, w, dimensionError, logData)
+
+		So(w.Code, ShouldEqual, http.StatusNotFound)
+		So(w.Body.String(), ShouldContainSubstring, dimensionError.Error())
+	})
+
+	Convey("Correctly handle bad request", t, func() {
+		w := httptest.NewRecorder()
+		dimensionError := errs.ErrUnableToParseJSON
+		logData := log.Data{"test": "bad request"}
+
+		handleDimensionErr(ctx, w, dimensionError, logData)
+
+		So(w.Code, ShouldEqual, http.StatusBadRequest)
+		So(w.Body.String(), ShouldContainSubstring, dimensionError.Error())
+	})
+
+	Convey("Correctly handle internal error", t, func() {
+		w := httptest.NewRecorder()
+		dimensionError := errs.ErrInternalServer
+		logData := log.Data{"test": "internal error"}
+
+		handleDimensionErr(ctx, w, dimensionError, logData)
+
+		So(w.Code, ShouldEqual, http.StatusInternalServerError)
+		So(w.Body.String(), ShouldContainSubstring, dimensionError.Error())
 	})
 }
