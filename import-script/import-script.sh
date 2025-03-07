22cabdcce79c00b47bc2e8c7ce5c7878a4980fa6
BarathaAberathne <barathaaberathne@gmail.com>
2025-03-07 09:04:07 +0000

    added  dataset and version record into datasets db

--- import-script/import-script.sh
+++ import-script/import-script.sh
@@ -4,3 +4,4 @@ mongoimport --db datasets --collection datasets --file datasets.json
 mongoimport --db datasets --collection editions --file editions.json
 mongoimport --db datasets --collection instances --file instances.json
 mongoimport --db datasets --collection dimension.options --file dimension.options.json
+mongoimport --db datasets --collection versions --file versions.json
