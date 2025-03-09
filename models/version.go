d4d0dc6f0b771251534902bffd8bb7153c2ae9c4
Jonathan Adcock <jonathan.adcock@and.digital>
2025-03-09 21:35:13 +0000

    added some commenting around version data model

--- models/version.go
+++ models/version.go
@@ -26,28 +26,28 @@ var (
 
 // Version represents information related to a single version for an edition of a dataset
 type Version struct {
+	ID                 string               `bson:"id,omitempty"                    json:"id,omitempty"`
 	Alerts             *[]Alert             `bson:"alerts,omitempty"                json:"alerts,omitempty"`
 	CollectionID       string               `bson:"collection_id,omitempty"         json:"collection_id,omitempty"`
 	DatasetID          string               `bson:"-"                               json:"dataset_id,omitempty"`
 	Dimensions         []Dimension          `bson:"dimensions,omitempty"            json:"dimensions,omitempty"`
+	Distributions      *[]Distribution      `bson:"distributions,omitempty"         json:"distributions,omitempty"`
 	Downloads          *DownloadList        `bson:"downloads,omitempty"             json:"downloads,omitempty"`
 	Edition            string               `bson:"edition,omitempty"               json:"edition,omitempty"`
-	Headers            []string             `bson:"headers,omitempty"               json:"-"`
-	ID                 string               `bson:"id,omitempty"                    json:"id,omitempty"`
+	ETag               string               `bson:"e_tag"                           json:"-"` // Not in swagger.yaml
+	Headers            []string             `bson:"headers,omitempty"               json:"-"` // Not in swagger.yaml
+	IsBasedOn          *IsBasedOn           `bson:"is_based_on,omitempty"           json:"is_based_on,omitempty"`
 	LastUpdated        time.Time            `bson:"last_updated,omitempty"          json:"last_updated,omitempty"`
 	LatestChanges      *[]LatestChange      `bson:"latest_changes,omitempty"        json:"latest_changes,omitempty"`
 	Links              *VersionLinks        `bson:"links,omitempty"                 json:"links,omitempty"`
+	LowestGeography    string               `bson:"lowest_geography,omitempty"      json:"lowest_geography,omitempty"`
+	QualityDesignation QualityDesignation   `bson:"quality_designation,omitempty"   json:"quality_designation,omitempty"`
 	ReleaseDate        string               `bson:"release_date,omitempty"          json:"release_date,omitempty"`
 	State              string               `bson:"state,omitempty"                 json:"state,omitempty"`
 	Temporal           *[]TemporalFrequency `bson:"temporal,omitempty"              json:"temporal,omitempty"`
-	UsageNotes         *[]UsageNote         `bson:"usage_notes,omitempty"           json:"usage_notes,omitempty"`
-	IsBasedOn          *IsBasedOn           `bson:"is_based_on,omitempty"           json:"is_based_on,omitempty"`
-	Version            int                  `bson:"version,omitempty"               json:"version,omitempty"`
 	Type               string               `bson:"type,omitempty"                  json:"type,omitempty"`
-	ETag               string               `bson:"e_tag"                           json:"-"`
-	LowestGeography    string               `bson:"lowest_geography,omitempty"      json:"lowest_geography,omitempty"`
-	QualityDesignation QualityDesignation   `bson:"quality_designation,omitempty"   json:"quality_designation,omitempty"`
-	Distributions      *[]Distribution      `bson:"distributions,omitempty"         json:"distributions,omitempty"`
+	UsageNotes         *[]UsageNote         `bson:"usage_notes,omitempty"           json:"usage_notes,omitempty"`
+	Version            int                  `bson:"version,omitempty"               json:"version,omitempty"`
 }
 
 // Alert represents an object containing information on an alert
