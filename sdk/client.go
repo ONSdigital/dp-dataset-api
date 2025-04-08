36a79a73b6e2786b8c51cc176d92bdef39c1738f
Jonathan Adcock <jonathan.adcock@and.digital>
2025-04-08 14:29:15 +0100

    split out some functions into a new file and rearranged others in client.go

--- sdk/client.go
+++ sdk/client.go
@@ -2,12 +2,9 @@ package sdk
 
 import (
 	"context"
-	"net/http"
 
 	"github.com/ONSdigital/dp-api-clients-go/v2/health"
 	"github.com/ONSdigital/dp-healthcheck/healthcheck"
-	dpNetRequest "github.com/ONSdigital/dp-net/v2/request"
-	"github.com/ONSdigital/log.go/v2/log"
 )
 
 const (
@@ -16,6 +13,21 @@ const (
 
 type Client struct {
 	hcCli *health.Client
+}
+
+// Checker calls topic api health endpoint and returns a check object to the caller
+func (c *Client) Checker(ctx context.Context, check *healthcheck.CheckState) error {
+	return c.hcCli.Checker(ctx, check)
+}
+
+// Health returns the underlying Healthcheck Client for this API client
+func (c *Client) Health() *health.Client {
+	return c.hcCli
+}
+
+// URL returns the URL used by this client
+func (c *Client) URL() string {
+	return c.hcCli.URL
 }
 
 // New creates a new instance of Client for the service
@@ -30,38 +42,5 @@ func New(datasetAPIUrl string) *Client {
 func NewWithHealthClient(hcCli *health.Client) *Client {
 	return &Client{
 		hcCli: health.NewClientWithClienter(service, hcCli.URL, hcCli.Client),
-	}
-}
-
-// URL returns the URL used by this client
-func (c *Client) URL() string {
-	return c.hcCli.URL
-}
-
-// Health returns the underlying Healthcheck Client for this API client
-func (c *Client) Health() *health.Client {
-	return c.hcCli
-}
-
-// Checker calls topic api health endpoint and returns a check object to the caller
-func (c *Client) Checker(ctx context.Context, check *healthcheck.CheckState) error {
-	return c.hcCli.Checker(ctx, check)
-}
-
-// Adds collectID to request header if not empty
-// TODO: Add to dp-net?
-func addCollectionIDHeader(r *http.Request, collectionID string) {
-	if collectionID != "" {
-		r.Header.Add(dpNetRequest.CollectionIDHeaderKey, collectionID)
-	}
-}
-
-// closeResponseBody closes the response body and logs an error if unsuccessful
-// TODO: Add to dp-net?
-func closeResponseBody(ctx context.Context, resp *http.Response) {
-	if resp.Body != nil {
-		if err := resp.Body.Close(); err != nil {
-			log.Error(ctx, "error closing http response body", err)
-		}
 	}
 }
--- sdk/client_test.go
+++ sdk/client_test.go
@@ -4,17 +4,13 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"errors"
 	"io"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/ONSdigital/dp-api-clients-go/v2/health"
 	"github.com/ONSdigital/dp-healthcheck/healthcheck"
 	dphttp "github.com/ONSdigital/dp-net/v2/http"
-	dpNetRequest "github.com/ONSdigital/dp-net/v2/request"
-	"github.com/ONSdigital/log.go/v2/log"
 	. "github.com/smartystreets/goconvey/convey"
 )
 
@@ -100,66 +96,5 @@ func TestHealthCheckerClient(t *testing.T) {
 			So(initialStateCheck.Name(), ShouldEqual, service)
 			So(initialStateCheck.Status(), ShouldEqual, healthcheck.StatusOK)
 		})
-	})
-}
-
-// Tests for the `addCollectionIDHeader` function
-func TestAddCollectionIDHeader(t *testing.T) {
-	mockRequest := httptest.NewRequest("GET", "/", http.NoBody)
-
-	Convey("If collectionID is empty string", t, func() {
-		collectionID := ""
-		Convey("Test `CollectionIDHeaderKey` field is not added to the request header", func() {
-			addCollectionIDHeader(mockRequest, collectionID)
-			So(mockRequest.Header.Values(dpNetRequest.CollectionIDHeaderKey), ShouldBeEmpty)
-		})
-	})
-
-	Convey("If collectionID is a valid string", t, func() {
-		collectionID := "1234"
-		Convey("Test `CollectionIDHeaderKey` field is set to the request header", func() {
-			addCollectionIDHeader(mockRequest, collectionID)
-			So(mockRequest.Header.Values(dpNetRequest.CollectionIDHeaderKey), ShouldNotBeEmpty)
-			So(mockRequest.Header.Get(dpNetRequest.CollectionIDHeaderKey), ShouldEqual, collectionID)
-		})
-	})
-}
-
-type mockReadCloser struct {
-	raiseError bool
-}
-
-// Implemented just to keep compiler happy for mock object
-func (m *mockReadCloser) Read(p []byte) (n int, err error) {
-	return 0, io.EOF
-}
-
-// Returns an error if `raiseError` is `true`, otherwise `false`
-func (m *mockReadCloser) Close() error {
-	if m.raiseError {
-		return errors.New("error closing body")
-	}
-	return nil
-}
-
-// Tests for `closeResponseBody` function
-func TestCloseResponseBody(t *testing.T) {
-	ctx := context.Background()
-	// Create a buffer to capture log output for tests
-	var buf bytes.Buffer
-	var fbBuf bytes.Buffer
-	log.SetDestination(&buf, &fbBuf)
-
-	Convey("Test function runs without logging an error if body.Close() completes without error", t, func() {
-		mockResponse := http.Response{Body: &mockReadCloser{raiseError: false}}
-
-		closeResponseBody(ctx, &mockResponse)
-		So(buf.String(), ShouldBeEmpty)
-	})
-	Convey("Test function logs an error if body.Close() returns an error", t, func() {
-		mockResponse := http.Response{Body: &mockReadCloser{raiseError: true}}
-
-		closeResponseBody(ctx, &mockResponse)
-		So(buf.String(), ShouldContainSubstring, "error closing http response body")
 	})
 }
