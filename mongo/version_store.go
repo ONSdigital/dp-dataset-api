e5710700fc96841892ac5ea8cd8ef9bf0974834a
aryan-patel <aryanpatel1771@gmail.com>
2025-06-19 09:59:16 +0100

    search for specifically static types

--- mongo/version_store.go
+++ mongo/version_store.go
@@ -85,7 +85,7 @@ func (m *Mongo) CheckEditionExistsStatic(ctx context.Context, id, editionID, sta
 // GetStaticVersionsByState retrieves all versions that match the provided state
 // If state is empty, the search will include any state that is not "published"
 func (m *Mongo) GetStaticVersionsByState(ctx context.Context, state string, offset, limit int) ([]*models.Version, int, error) {
-	filter := bson.M{}
+	filter := bson.M{"type": models.Static.String()}
 
 	if state == "" {
 		filter["state"] = bson.M{"$ne": models.PublishedState}
