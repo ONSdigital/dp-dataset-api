4946302c242d4299e1bfbedb753ccc043538bb45
aryan-patel <aryanpatel1771@gmail.com>
2025-09-11 12:50:14 +0100

    update lint.sh script to v2

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -3,6 +3,6 @@
 cwd=$(pwd)
 
 pushd $cwd/dp-dataset-api
-  go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.6
+  go install github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v2.4.0
   make lint
 popd
