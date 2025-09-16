46f418fe5842025d4bea17d8b2dc4f9b920a902c
aryan-patel <aryanpatel1771@gmail.com>
2025-09-16 09:08:29 +0100

    setup code to allow unit testing of mongo functions

--- go.mod
+++ go.mod
@@ -17,6 +17,7 @@ require (
 	github.com/ONSdigital/dp-graph/v2 v2.18.0
 	github.com/ONSdigital/dp-healthcheck v1.6.4
 	github.com/ONSdigital/dp-kafka/v4 v4.2.0
+	github.com/ONSdigital/dp-mongodb-in-memory v1.8.1
 	github.com/ONSdigital/dp-mongodb/v3 v3.8.0
 	github.com/ONSdigital/dp-net/v3 v3.4.0
 	github.com/ONSdigital/dp-otel-go v0.0.8
@@ -40,7 +41,6 @@ require (
 require (
 	github.com/ONSdigital/dis-redis v0.3.0 // indirect
 	github.com/ONSdigital/dp-authorisation/v2 v2.32.2 // indirect
-	github.com/ONSdigital/dp-mongodb-in-memory v1.8.1 // indirect
 	github.com/ONSdigital/dp-net/v2 v2.22.0 // indirect
 	github.com/ONSdigital/dp-permissions-api v1.0.0 // indirect
 	github.com/ONSdigital/golang-neo4j-bolt-driver v0.0.0-20241121114036-9f4b82bb9d37 // indirect
---
+++ mongo/version_store_test.go
@@ -0,0 +1,52 @@
+package mongo
+
+import (
+	"context"
+	"testing"
+
+	errs "github.com/ONSdigital/dp-dataset-api/apierrors"
+	"github.com/ONSdigital/dp-dataset-api/config"
+	"github.com/ONSdigital/dp-dataset-api/models"
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestUpdateVersionStatic(t *testing.T) {
+	Convey("Given a current version, version update and etag", t, func() {
+		ctx := context.Background()
+
+		mongoDB, _, err := getTestMongoDB(ctx)
+		So(err, ShouldBeNil)
+
+		_, err = setupVersionsTestData(ctx, mongoDB)
+		So(err, ShouldBeNil)
+
+		currentVersion := &models.Version{ID: "version1", Edition: "edition1", Version: 1, ETag: "version1ETag"}
+		versionUpdate := &models.Version{EditionTitle: "First Edition Updated"}
+		oldETag := "version1ETag"
+
+		Convey("When UpdateVersionStatic is called and the version exists", func() {
+			newEtag, err := mongoDB.UpdateVersionStatic(ctx, currentVersion, versionUpdate, oldETag)
+
+			Convey("Then the version is updated successfully", func() {
+				So(err, ShouldBeNil)
+				So(newEtag, ShouldNotEqual, oldETag)
+			})
+
+			Convey("And the version is updated in the database", func() {
+				var updatedVersion models.Version
+				err = mongoDB.Connection.Collection(mongoDB.ActualCollectionName(config.VersionsCollection)).FindOne(ctx, map[string]string{"id": currentVersion.ID}, &updatedVersion)
+				So(err, ShouldBeNil)
+				So(updatedVersion.EditionTitle, ShouldEqual, "First Edition Updated")
+				So(updatedVersion.ETag, ShouldEqual, newEtag)
+			})
+		})
+
+		Convey("When UpdateVersionStatic is called and the version does not exist", func() {
+			_, err := mongoDB.UpdateVersionStatic(ctx, &models.Version{}, versionUpdate, oldETag)
+
+			Convey("Then a VersionNotFound error is returned", func() {
+				So(err, ShouldEqual, errs.ErrVersionNotFound)
+			})
+		})
+	})
+}
