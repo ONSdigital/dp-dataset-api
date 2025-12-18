4f9f4d95b255737a004005364e1eb7d548fa2e64
aryan-patel <aryanpatel1771@gmail.com>
2025-12-18 10:41:19 +0000

    add api router public url to builder

--- url/builder_test.go
+++ url/builder_test.go
@@ -15,6 +15,7 @@ var (
 	downloadServiceURL = &neturl.URL{Scheme: "http", Host: "localhost:23600"}
 	importAPIURL       = &neturl.URL{Scheme: "http", Host: "localhost:21800"}
 	websiteURL         = &neturl.URL{Scheme: "http", Host: "localhost:20000"}
+	apiRouterPublicURL = &neturl.URL{Scheme: "http", Host: "localhost:23200", Path: "v1"}
 )
 
 const (
@@ -25,7 +26,7 @@ const (
 
 func TestBuilder_BuildWebsiteDatasetVersionURL(t *testing.T) {
 	Convey("Given a URL builder", t, func() {
-		urlBuilder := url.NewBuilder(websiteURL, downloadServiceURL, datasetAPIURL, codeListAPIURL, importAPIURL)
+		urlBuilder := url.NewBuilder(websiteURL, downloadServiceURL, datasetAPIURL, codeListAPIURL, importAPIURL, apiRouterPublicURL)
 
 		Convey("When BuildWebsiteDatasetVersionURL is called", func() {
 			builtURL := urlBuilder.BuildWebsiteDatasetVersionURL(datasetID, edition, version)
@@ -40,9 +41,23 @@ func TestBuilder_BuildWebsiteDatasetVersionURL(t *testing.T) {
 	})
 }
 
+func TestBuilder_GetWebsiteURL(t *testing.T) {
+	Convey("Given a URL builder", t, func() {
+		urlBuilder := url.NewBuilder(websiteURL, downloadServiceURL, datasetAPIURL, codeListAPIURL, importAPIURL, apiRouterPublicURL)
+
+		Convey("When GetWebsiteURL is called", func() {
+			returnedURL := urlBuilder.GetWebsiteURL()
+
+			Convey("Then the expected URL is returned", func() {
+				So(returnedURL, ShouldEqual, websiteURL)
+			})
+		})
+	})
+}
+
 func TestBuilder_GetDownloadServiceURL(t *testing.T) {
 	Convey("Given a URL builder", t, func() {
-		urlBuilder := url.NewBuilder(websiteURL, downloadServiceURL, datasetAPIURL, codeListAPIURL, importAPIURL)
+		urlBuilder := url.NewBuilder(websiteURL, downloadServiceURL, datasetAPIURL, codeListAPIURL, importAPIURL, apiRouterPublicURL)
 
 		Convey("When GetDownloadServiceURL is called", func() {
 			returnedURL := urlBuilder.GetDownloadServiceURL()
@@ -56,7 +71,7 @@ func TestBuilder_GetDownloadServiceURL(t *testing.T) {
 
 func TestBuilder_GetDatasetAPIURL(t *testing.T) {
 	Convey("Given a URL builder", t, func() {
-		urlBuilder := url.NewBuilder(websiteURL, downloadServiceURL, datasetAPIURL, codeListAPIURL, importAPIURL)
+		urlBuilder := url.NewBuilder(websiteURL, downloadServiceURL, datasetAPIURL, codeListAPIURL, importAPIURL, apiRouterPublicURL)
 
 		Convey("When GetDatasetAPIURL is called", func() {
 			returnedURL := urlBuilder.GetDatasetAPIURL()
@@ -70,7 +85,7 @@ func TestBuilder_GetDatasetAPIURL(t *testing.T) {
 
 func TestBuilder_GetCodeListAPIURL(t *testing.T) {
 	Convey("Given a URL builder", t, func() {
-		urlBuilder := url.NewBuilder(websiteURL, downloadServiceURL, datasetAPIURL, codeListAPIURL, importAPIURL)
+		urlBuilder := url.NewBuilder(websiteURL, downloadServiceURL, datasetAPIURL, codeListAPIURL, importAPIURL, apiRouterPublicURL)
 
 		Convey("When GetCodeListAPIURL is called", func() {
 			returnedURL := urlBuilder.GetCodeListAPIURL()
@@ -84,13 +99,27 @@ func TestBuilder_GetCodeListAPIURL(t *testing.T) {
 
 func TestBuilder_GetImportAPIURL(t *testing.T) {
 	Convey("Given a URL builder", t, func() {
-		urlBuilder := url.NewBuilder(websiteURL, downloadServiceURL, datasetAPIURL, codeListAPIURL, importAPIURL)
+		urlBuilder := url.NewBuilder(websiteURL, downloadServiceURL, datasetAPIURL, codeListAPIURL, importAPIURL, apiRouterPublicURL)
 
 		Convey("When GetImportAPIURL is called", func() {
 			returnedURL := urlBuilder.GetImportAPIURL()
 
 			Convey("Then the expected URL is returned", func() {
 				So(returnedURL, ShouldEqual, importAPIURL)
+			})
+		})
+	})
+}
+
+func TestBuilder_GetAPIRouterPublicURL(t *testing.T) {
+	Convey("Given a URL builder", t, func() {
+		urlBuilder := url.NewBuilder(websiteURL, downloadServiceURL, datasetAPIURL, codeListAPIURL, importAPIURL, apiRouterPublicURL)
+
+		Convey("When GetAPIRouterPublicURL is called", func() {
+			returnedURL := urlBuilder.GetAPIRouterPublicURL()
+
+			Convey("Then the expected URL is returned", func() {
+				So(returnedURL, ShouldEqual, apiRouterPublicURL)
 			})
 		})
 	})
